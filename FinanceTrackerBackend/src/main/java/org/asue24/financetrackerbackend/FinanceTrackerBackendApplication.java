e889316f4b3e85b6c4dfc272e1a8a071f71acee0
Ahtat204 <lahcen28ahtat@gmail.com>
2025-08-24 21:31:51 +0200

    initial commit

---
+++ FinanceTrackerBackend/src/main/java/org/asue24/financetrackerbackend/FinanceTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.asue24.financetrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceTrackerBackendApplication.class, args);
+    }
+
+}
