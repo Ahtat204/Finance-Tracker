5879c4fe97bc833b367e26a049796aa70c70b4fd
Lahcen <lahcen28ahtat@gmail.com>
2026-03-15 18:01:37 +0100

    update,added .gitignore and Login function

--- FinanceTrackerFrontEnd/src/main/java/org/asue24/financetrackerfrontend/services/AuthenticationService.kt
+++ FinanceTrackerFrontEnd/src/main/java/org/asue24/financetrackerfrontend/services/AuthenticationService.kt
@@ -1,7 +1,16 @@
 package org.asue24.financetrackerfrontend.services
-import khttp.delete as httpDelete
-
+import khttp.post
+import khttp.responses.Response
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.asue24.financetrackerfrontend.model.dto.login.LoginRequest
+import org.asue24.financetrackerfrontend.model.dto.login.LoginResponse
+import org.json.JSONObject
 
 class AuthenticationService {
-
+fun Authenticate(request: LoginRequest,url:String):LoginResponse{
+    val response: Response = post(url, json = mapOf("email" to request.email,"password" to request.password))
+    val rawJson: JSONObject = response.jsonObject
+    val mapper = ObjectMapper()
+    return mapper.readValue(rawJson.toString(), LoginResponse::class.java)
+}
 }
